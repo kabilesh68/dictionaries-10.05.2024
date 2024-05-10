13c4da34ebcc35db8e037b3080b6479b3ad20f5c
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-10 11:20:55 +0530

    Update 71772312141-kabilesh(g) dict.py

--- 71772312141-kabilesh(g) dict.py
+++ 71772312141-kabilesh(g) dict.py
@@ -6,3 +6,7 @@ marks['raka']['maths']='77'
 print(marks)
 marks=dict(sorted(marks.items()))
 print(marks)
+output:
+marks obtained by amol in english: 68
+{'subu': {'maths': 88, 'Eng': 60, 'SSt': 95}, 'Amol': {'maths': 78, 'Eng': 68, 'SSt': 89}, 'raka': {'maths': '77', 'Eng': 66, 'SSt': 77}}
+{'Amol': {'maths': 78, 'Eng': 68, 'SSt': 89}, 'raka': {'maths': '77', 'Eng': 66, 'SSt': 77}, 'subu': {'maths': 88, 'Eng': 60, 'SSt': 95}}
